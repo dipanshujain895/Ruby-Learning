3c5753b9b0a8f5265bbb138a2d18763a1db87067
Dipanshu Jain <dipanshu.jain@1mg.com>
2024-04-02 19:55:18 +0530

    Added Hashing

---
+++ hashes.rb
@@ -0,0 +1,18 @@
+# Hashes in ruby are same as dictionary in python
+# Hashes are data structure containing key-value pairs
+
+# Keys are unique, Values can be same for multiple keys
+# Hash values are extracted using keys and not by the order
+
+# Hashes are represented by {}
+# Example of printing a hash {key => value, key => value, key => value}
+
+hash = {}
+
+hash["a"] = "apple"
+hash["b"] = "ball"
+hash["c"] = "cat"
+
+p hash
+puts hash
+puts hash.class
